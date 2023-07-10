ba78cfccc4001d6973c21a02ee2006e8dfe32fa0
brf153 <153hsb@gmail.com>
2023-07-10 07:41:42 +0530

    connected to database

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incoming Routes, *gin.Engine){
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+}
--- routes/userRouter.go
+++ routes/userRouter.go
@@ -1,2 +1,13 @@
 package routes
 
+import (
+	controller "github.com/brf153/jwt-golang/controllers"
+	"github.com/brf153/jwt-golang/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine){
+	incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.GET("/users",controller.GetUsers())
+	incomingRoutes.GET("./users/:user_id", controller.GetUser())
+}
