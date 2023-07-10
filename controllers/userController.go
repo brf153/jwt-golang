3e912ddd7526cebc80c871956e98e9ef0bb17834
brf153 <153hsb@gmail.com>
2023-07-10 10:57:46 +0530

    still hustling

--- controllers/userController.go
+++ controllers/userController.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"github.com/brf153/jwt-golang.git/database"
+	"github.com/brf153/jwt-golang.git/models"
 	helper "github.com/brf153/jwt-golang/helpers"
 	"github.com/brf153/jwt-golang/models"
 	"github.com/gin-gonic/gin"
@@ -23,10 +24,42 @@ func HashPassword()
 
 func VerifyPassword()
 
-func SignUp()
+func SignUp()gin.HandlerFunc{
+	return func(c *gin.Context){
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+
+		if err := c.BindJSON(&user); err!=nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		validationErr := validate.Struct(user)
+		if validationErr != nil{
+			c.JSON(http.StatusBadRequest, )
+		}
+	}
+}
 
 func Login()
 
 func GetUsers()
 
-func GetUser()
+func GetUser() gin.HandlerFunc{
+	return func(c *gin.Context){
+		userId := c.Param("user_id")
+		if err:= helper.MatchUserTypeToUid(c , userId); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H("error":err.Error()))
+			return 
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id":userId}).Decode(&user)
+		defer cancel()
+		if err!= nil{
+			c.JSON(http.StatusInternalServerError, gin.M{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+	}
+}
